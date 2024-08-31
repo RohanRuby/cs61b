45172e1a4b458352273c655b389bef0f0e2a0cde
RohanRuby <liruohan1997@gmail.com>
2024-08-31 16:09:48 +0200

    proj1 modify ArrayDeque

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -14,6 +14,7 @@ public class ArrayDeque<T> {
         nextFirst = defaultLength/2;
         nextLast = nextFirst + 1;
     }
+    /*
     private void resize(int capacity){
         T[] newArray = (T[]) new Object[capacity];
         for(int i = 0; i < size; i++)
@@ -64,7 +65,52 @@ public class ArrayDeque<T> {
         return val;
 
     }
+*/
+    private void resize(int capacity){
+        T[] newArray = (T[]) new Object[capacity];
+        int ptr = getIndexPlus(nextFirst);
+        int i = 0;
+        while(ptr != nextLast || i == 0 ){
+            newArray[i] = array[ptr];
+            ptr = getIndexPlus(ptr);
+            i++;
+        }
+        array = newArray;
+        nextFirst = array.length - 1;
+        nextLast = size;
+    }
 
+    public void addFirst(T val){
+        if(size == array.length)
+            resize(array.length*2);
+        array[nextFirst] = val;
+        size++;
+        nextFirst = getIndexMinus(nextFirst);
+    }
+
+    public void addLast(T val){
+        if(size == array.length)
+            resize(array.length*2);
+        array[nextLast] = val;
+        size++;
+        nextLast = getIndexPlus(nextLast);
+    }
+
+    public T removeFirst(){
+        if (this.size() < array.length/4 && array.length > 16) resize(array.length/4);
+        T val = array[getIndexPlus(nextFirst)];
+        size--;
+        nextFirst = getIndexPlus(nextFirst);
+        return val;
+    }
+
+    public T removeLast(){
+        if (this.size() < array.length/4 && array.length > 16) resize(array.length/4);
+        T val = array[getIndexMinus(nextLast)];
+        size--;
+        nextLast = getIndexMinus(nextLast);
+        return val;
+    }
     public int size(){
         return size;
     }
@@ -74,12 +120,26 @@ public class ArrayDeque<T> {
     }
 
     public void printDeque(){
-        for(int i = nextFirst + 1; i< nextLast; i++){
-            System.out.println(array[i]);
+        int ptr = getIndexPlus(nextFirst);
+        int total = size;
+        while(ptr != nextLast || total > 0){
+            System.out.print(array[ptr] + " ");
+            ptr = getIndexPlus(ptr);
+            total--;
         }
     }
 
     public T get(int i){
         return array[i];
+    }
+
+    private int getIndexMinus(int index){
+        if(index == 0) return array.length - 1;
+        return index - 1;
+    }
+
+    private int getIndexPlus(int index){
+        if(index == array.length - 1) return 0;
+        return index + 1;
     }
 }
