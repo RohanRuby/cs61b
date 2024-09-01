48552db87bb3f5950c6d22a4f20c936af9d03a2a
RohanRuby <liruohan1997@gmail.com>
2024-09-01 20:36:37 +0200

    proj1 modify ArrayDeque

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -14,58 +14,7 @@ public class ArrayDeque<T> {
         nextFirst = defaultLength/2;
         nextLast = nextFirst + 1;
     }
-    /*
-    private void resize(int capacity){
-        T[] newArray = (T[]) new Object[capacity];
-        for(int i = 0; i < size; i++)
-            newArray[i] = array[i % capacity];
-        nextLast = size;
-        nextFirst = capacity - 1;
-        array = newArray;
-    }
-    public void addFirst(T val){
-        if(size == array.length)
-            resize(array.length*2);
-        array[nextFirst] = val;
-        size++;
-        nextFirst--;
-        if(nextFirst == -1) nextFirst = array.length - 1;
-    }
 
-    public void addLast(T val){
-        if(size == array.length)
-            resize(array.length*2);
-        //System.out.println("nextlast: " + nextLast + " length: " + array.length);
-
-        array[nextLast] = val;
-        size++;
-        nextLast++;
-        nextLast = nextLast % array.length;
-    }
-
-    public T removeFirst(){
-        if (this.size() < array.length/4) resize(array.length/4);
-        if (this.isEmpty()) return null;
-        int index = (nextFirst + 1) % array.length;
-        T val = array[index];
-        nextFirst++;
-        nextFirst = nextFirst % array.length;
-        size--;
-        return val;
-    }
-
-    public T removeLast(){
-        if (this.size() < array.length/4) resize( array.length/4);
-        if (this.isEmpty()) return null;
-        int index = nextLast - 1;
-        if(index < 0) index = index + array.length;
-        T val = array[index];
-        nextLast = index;
-        size--;
-        return val;
-
-    }
-*/
     private void resize(int capacity){
         T[] newArray = (T[]) new Object[capacity];
         int ptr = getIndexPlus(nextFirst);
@@ -97,6 +46,7 @@ public class ArrayDeque<T> {
     }
 
     public T removeFirst(){
+        if(isEmpty()) return null;
         if (this.size() < array.length/4 && array.length > 16) resize(array.length/4);
         T val = array[getIndexPlus(nextFirst)];
         size--;
@@ -105,6 +55,7 @@ public class ArrayDeque<T> {
     }
 
     public T removeLast(){
+        if(isEmpty()) return null;
         if (this.size() < array.length/4 && array.length > 16) resize(array.length/4);
         T val = array[getIndexMinus(nextLast)];
         size--;
